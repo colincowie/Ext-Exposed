20958a096b175b45848f3c547a5835f289706091
Colin Cowie <ccowie@purdue.edu>
2020-11-08 11:42:57 -0500

    improved source file viewing

--- crx_hunt.py
+++ crx_hunt.py
@@ -543,4 +543,4 @@ if __name__ == '__main__':
         load_es()
     db.create_all()
     create_es()
-    app.run(host="0.0.0.0",port=8080,debug=False)
+    app.run(host="0.0.0.0",port=8080,debug=True)
